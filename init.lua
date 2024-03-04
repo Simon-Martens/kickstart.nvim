e7ebc255693dceeaaf3854eb7ad842342dfc47a2
Simon Martens <simonmartens@mailbox.org>
2024-03-04 22:20:34 +0100

    Set languages for treestiter

--- init.lua
+++ init.lua
@@ -802,7 +802,7 @@ require('lazy').setup {
 
       ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup {
-        ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc' },
+        ensure_installed = { 'typescript', 'go', 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc' },
         -- Autoinstall languages that are not installed
         auto_install = true,
         highlight = { enable = true },
