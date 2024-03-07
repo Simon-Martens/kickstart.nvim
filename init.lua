dfd53dd2d101c68f0aa39a07408e8765efdc5c17
Simon Martens <simonmartens@mailbox.org>
2024-03-07 02:14:49 +0100

    Added a Problem Overview window

--- init.lua
+++ init.lua
@@ -901,6 +901,36 @@ require('lazy').setup {
     end,
   },
 
+  -- Plugin for notifications
+  -- 'rcarriga/nvim-notify',
+  --
+  -- Plugin for a diagnosics window
+  {
+    'folke/trouble.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
+
+  -- Noice is a plugin to get a command box like v*code has
+  -- {
+  --   'folke/noice.nvim',
+  --   event = 'VeryLazy',
+  --   opts = {
+  --     -- add any options here
+  --   },
+  --   dependencies = {
+  --     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+  --     'MunifTanjim/nui.nvim',
+  --     -- OPTIONAL:
+  --     --   `nvim-notify` is only needed, if you want to use the notification view.
+  --     --   If not available, we use `mini` as the fallback
+  --     'rcarriga/nvim-notify',
+  --   },
+  -- },
   -- Always keep the cursor vertically centered
   -- {
   --   'arnamak/stay-centered.nvim',
